8d7b418faf278d62e22c926c9f57d0ab92a1ac59
Anton Erememko <anton@pave.com>
2020-08-22 10:40:46 -0400

    improved Palindrome search

--- src/main/scala/palindrome.scala
+++ src/main/scala/palindrome.scala
@@ -1,66 +1,23 @@
 object Palindrome {
-//  def isPalindrome(s: List[Char]): Boolean = {
-//    def go(a: Array[Char], front: Int, end: Int):Boolean = {
-//      if(front >= end ) true
-//      else
-//        if (a(front) != a(end)) false
-//        else
-//          go(a, front + 1, end - 1)
-//    }
-//
-//    if(s.isEmpty) false
-//    else go(s.toArray, 0, s.length - 1)
-//  }
-
-//  def gen(head:List[Char], tail: List[Char]): List[String] = {
-//    tail match {
-//      case Nil if isPalindrome(head) => List((head).reverse.mkString)
-//      case h::t if isPalindrome(head) =>
-//        gen(List(h), t).map(s => (head).reverse.mkString + "|" + s.mkString) ++ gen(h :: head, t)
-//      case h::t => gen(h :: head, t)
-//      case Nil => Nil
-//    }
-//  }
-
-
-  def apply(s: String): List[String] = {
-    val m = getPalindromsMap(s.toCharArray)
-    build(0, m)
-  }
-
   def longestPalindrome(s: String): String = {
-    val m = getPalindromsMap(s.toCharArray)
-    val res = build(0, m).filterNot(_ == s)
-    res.maxBy(_.size)
-  }
-
-  def build(i: Int, m: Map[Int, List[Pali]]) : List[String] = {
-    val x = m.getOrElse(i, List())
-
-    if(x.isEmpty) List("")
-    else x.flatMap(s => build(s.end, m).map(a => s.text + a))
-  }
-
-  case class Pali(start: Int, end: Int, text: String)
-
-  def checkForPalindroms(a: Array[Char], s1: Int, s2: Int, acc: List[Pali]): List[Pali] = {
-    if(s1 < 0 || s2 >= a.size) {
-      acc
-    }
-    else if (a(s1) == a(s2)){
-      val p = Pali(s1, s2+1, a.slice(s1, s2+1).mkString)
-      checkForPalindroms(a, s1-1, s2+1, p::acc)
-    }
+    if(s.isEmpty) ""
     else {
-      acc
+      val a = s.toArray
+      val r = Range(0, s.size)
+      val pl = r.foldRight(Nil:List[(Int, Int)]){(i, acc) =>
+        expandAroundCenter(a, i, i, (i, i))::expandAroundCenter(a, i, i+1, (i, i))::acc
+      }
+      val z = pl.maxBy(x => Math.abs(x._2 - x._1))
+      a.slice(z._1, z._2+1).mkString
     }
   }
 
-  def getPalindromsMap(s: Array[Char]): Map[Int, List[Pali]] = {
-    val r = Range(0, s.size)
-    val pl = r.foldRight(Nil:List[Pali]){(i, acc) =>
-      checkForPalindroms(s, i, i, checkForPalindroms(s, i, i+1, acc))
+  // find the longest palindromic string starting from s1 and s2
+  def expandAroundCenter(a: Array[Char], s1: Int, s2: Int, acc: (Int, Int)): (Int, Int) = {
+    if (s1 < 0 || s2 >= a.size || a(s1) != a(s2)) {
+      acc
+    } else {
+      expandAroundCenter(a, s1 - 1, s2 + 1, (s1, s2))
     }
-    pl.groupBy(x=>x.start)
   }
 }
--- src/test/scala/PalindromeSpec.scala
+++ src/test/scala/PalindromeSpec.scala
@@ -1,45 +1,16 @@
 import org.scalatest.{FlatSpec, Matchers}
 
 class PalindromeSpec extends FlatSpec with Matchers {
-//  "Palindrome.isPalindrome" should "return true if a string is a palindrome" in {
-//    assert(Palindrome.isPalindrome("a".toList))
-//    assert(Palindrome.isPalindrome("aa".toList))
-//    assert(Palindrome.isPalindrome("aba".toList))
-//    assert(Palindrome.isPalindrome("abcba".toList))
-//  }
-//
-//  "Palindrome.isPalindrome" should "return false if a string is empty" in {
-//    Palindrome.isPalindrome("".toList) shouldBe false
-//  }
-//
-//  "Palindrome.isPalindrome" should "return false if a string is not a palindrome " in {
-//    Palindrome.isPalindrome("ab".toList) shouldBe false
-//    Palindrome.isPalindrome("abb".toList) shouldBe false
-//    Palindrome.isPalindrome("aba".toList) shouldBe true
-//  }
-//
-//  "Palindrome.apply" should "return list of all palindroms" in {
-//    Palindrome("ab").size should be(1)
-//    Palindrome("aa").size should be(2)
-//    Palindrome("abba").size should be(3)
-//    Palindrome("aabaa").size should be(6)
-//    Palindrome("aaaa").size should be(8)
-//  }
-//
-//  "Palindrome.apply" should "return the same result as oldApply" in {
-//    Palindrome("ab").size should be(Palindrome.oldApply("ab").size)
-//    Palindrome("aa").size should be(Palindrome.oldApply("aa").size)
-//    Palindrome("abba").size should be(Palindrome.oldApply("abba").size)
-//    Palindrome("aabaa").size should be(Palindrome.oldApply("aabaa").size)
-//    Palindrome("aaaa").size should be(Palindrome.oldApply("aaaa").size)
-//  }
+  "Palindrome.longestPalindrome" should "return the empty sting on empty input" in {
+    Palindrome.longestPalindrome("") should be("")
+  }
 
-  "Palindrome.checkForPalindroms" should "return list of palindroms" in {
-    Palindrome.checkForPalindroms("ab".toCharArray, 0, 0, Nil).size should be(1)
-    Palindrome.checkForPalindroms("ab".toCharArray, 0, 1, Nil).size should be(0)
-    Palindrome.checkForPalindroms("aa".toCharArray, 0, 1, Nil).size should be(1)
-    Palindrome.checkForPalindroms("ab".toCharArray, 1, 1, Nil).size should be(1)
-    Palindrome.checkForPalindroms("aaaaaaa".toCharArray, 3, 4, Nil).size should be(3)
-    Palindrome.checkForPalindroms("aaaaaaa".toCharArray, 3, 3, Nil).size should be(4)
+  "Palindrome.longestPalindrome" should "return the one symbol sting on non-palindromic input" in {
+    Palindrome.longestPalindrome("abcdef").size should be(1)
+  }
+
+  "Palindrome.longestPalindrome" should "return the longest palindromic string" in {
+    Palindrome.longestPalindrome("babacd") should be("bab")
   }
 }
+
